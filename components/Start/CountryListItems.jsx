732f4654d0725d9e212b58ef11817fe267c060e9
Alisina Barghandan <barghandanalisina@gmail.com>
2022-10-21 04:07:18 +0530

    finishing bottom sheet conf.

---
+++ components/Start/CountryListItems.jsx
@@ -0,0 +1,11 @@
+// UI's
+import {  } from '@ui-kitten/components';
+
+const CountryListItems = (props) => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default CountryListItems;
--- components/Start/CountryLists.jsx
+++ components/Start/CountryLists.jsx
@@ -1,46 +1,47 @@
 import { useEffect } from "react";
 import { ScrollView, View } from "react-native";
 
-// UI's
-import { Text } from "@ui-kitten/components";
-
 // Redux
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCountryLists } from "../../redux/CountriesList/actions";
+import { setToggle } from '../../redux/WelcomePageToggle/actions';
 
 // Bottom Sheet
 import BottomSheet from "react-native-simple-bottom-sheet";
 
+// Other Components
+import CountryListItems from "./CountryListItems";
+
 const CountryLists = () => {
   useEffect(() => {
     dispatch(fetchCountryLists());
-    console.log(countryData);
   }, [toggleStatus]);
 
   // Redux Operations
   const { countryData } = useSelector((state) => state.countryDataReducer);
   const { toggleStatus } = useSelector((state) => state.toggleStatusReducer);
   const dispatch = useDispatch();
+  // Function to call the setToggle action for every onClose event on the bottom sheet
+  const setToggleOnClose = () => {
+    dispatch(setToggle())
+  }
 
   return (
     <View>
-      {!toggleStatus ? (
-        <></>
-      ) : (
-        <BottomSheet sliderMinHeight={0} sliderMaxHeight={600}>
-          {(onScrollEndDrag) => (
-            <ScrollView onScrollEndDrag={onScrollEndDrag}>
-              {countryData.length !== 0 ? (
-                <View>
-                  {countryData.map((country, index) => (
-                    <Text key={index + Math.random()}>{country.name}</Text>
-                  ))}
-                </View>
-              ) : null}
-            </ScrollView>
-          )}
-        </BottomSheet>
-      )}
+      {toggleStatus ? (
+        <BottomSheet onClose={() => setToggleOnClose()} sliderMinHeight={0} sliderMaxHeight={600}>
+        {(onScrollEndDrag) => (
+          <ScrollView showsVerticalScrollIndicator={false} onScrollEndDrag={onScrollEndDrag}>
+              <View>
+                {countryData.map((country) => (
+                  <CountryListItems />
+                ))}
+              </View>
+          </ScrollView>
+        )}
+      </BottomSheet>
+      ) : null
+      }
     </View>
   );
 };
