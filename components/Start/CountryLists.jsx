c077db49fa74eacf25b48613f3402efb61f1a5f9
Alisina Barghandan <barghandanalisina@gmail.com>
2022-10-21 03:43:56 +0530

    Update CountryLists.jsx

--- components/Start/CountryLists.jsx
+++ components/Start/CountryLists.jsx
@@ -1,41 +1,48 @@
-import { useEffect } from 'react';
-import { ScrollView, View } from 'react-native';
+import { useEffect } from "react";
+import { ScrollView, View } from "react-native";
 
 // UI's
-import { Text } from '@ui-kitten/components';
+import { Text } from "@ui-kitten/components";
 
 // Redux
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchCountryLists } from '../../redux/CountriesList/actions';
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCountryLists } from "../../redux/CountriesList/actions";
 
 // Bottom Sheet
-import BottomSheet from 'react-native-simple-bottom-sheet';
+import BottomSheet from "react-native-simple-bottom-sheet";
 
 const CountryLists = () => {
-    useEffect(() => {
-      dispatch(fetchCountryLists());
-      console.log(countryData);
-    }, [toggleStatus]);
+  useEffect(() => {
+    dispatch(fetchCountryLists());
+    console.log(countryData);
+  }, [toggleStatus]);
 
-    // Redux Operations
-    const { countryData } = useSelector((state) => state.countryDataReducer);
-    const { toggleStatus } = useSelector((state) => state.toggleStatusReducer);
-    const dispatch = useDispatch();
+  // Redux Operations
+  const { countryData } = useSelector((state) => state.countryDataReducer);
+  const { toggleStatus } = useSelector((state) => state.toggleStatusReducer);
+  const dispatch = useDispatch();
 
-
-    return (
-      <View>
-          <BottomSheet sliderMinHeight={50} sliderMaxHeight={300}>
+  return (
+    <View>
+      {!toggleStatus ? (
+        <></>
+      ) : (
+        <BottomSheet sliderMinHeight={0} sliderMaxHeight={600}>
           {(onScrollEndDrag) => (
-          <ScrollView onScrollEndDrag={onScrollEndDrag}>
-            {
-              countryData.length !== 0 ? (<View>{countryData.map((country, index) => (<Text key={index + Math.random()}>{country.name}</Text>))}</View>) : null
-            }
-          </ScrollView>
-        )}
-          </BottomSheet>
-        </View>
-    )
-}
+            <ScrollView onScrollEndDrag={onScrollEndDrag}>
+              {countryData.length !== 0 ? (
+                <View>
+                  {countryData.map((country, index) => (
+                    <Text key={index + Math.random()}>{country.name}</Text>
+                  ))}
+                </View>
+              ) : null}
+            </ScrollView>
+          )}
+        </BottomSheet>
+      )}
+    </View>
+  );
+};
 
 export default CountryLists;
